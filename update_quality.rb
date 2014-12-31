95ccdfe28a60fbccd4f5425835ed48dabe84fd08
Dave Newton <davelnewton@gmail.com>
2014-12-31 08:21:54 -0600

    Calculators now take award as ctor arg, starting to move some func into evil base class

--- update_quality.rb
+++ update_quality.rb
@@ -3,30 +3,45 @@ require 'award'
 class AwardQualityCalculator
   def initialize
     @calculators = {
-      "Blue Distinction Plus" => BlueDistinctionPlus.new,
-      "Blue First" => BlueFirst.new,
-      "Blue Compare" => BlueCompare.new
+      "Blue Distinction Plus" => BlueDistinctionPlus,
+      "Blue First" => BlueFirst,
+      "Blue Compare" => BlueCompare
     }
   end
 
   def update(award)
-    calculator = @calculators.fetch(award.name) { Other.new }
-    calculator.update(award)
+    calculator = @calculators.fetch(award.name, Other)
+    calculator.new(award).update
+  end
+end
+
+class AwardCalculator
+  def initialize(award)
+    @award = award
+  end
+
+  def expired?
+    @award.expires_in < 0
+  end
+
+  # Default award blows up: enforce subclass impl.
+  def update
+    raise 'Unimplemented award type update!'
   end
 end
 
 # Unknown/other names.
-class Other
-  def update(award)
-    if award.quality > 0
-      award.quality -= 1
+class Other < AwardCalculator
+  def update
+    if @award.quality > 0
+      @award.quality -= 1
     end
 
-    award.expires_in -= 1
+    @award.expires_in -= 1
 
-    if award.expires_in < 0
-      if award.quality > 0
-        award.quality -= 1
+    if @award.expires_in < 0
+      if @award.quality > 0
+        @award.quality -= 1
       end
     end
   end
@@ -34,62 +49,68 @@ end
 
 # Blue Distinction Plus: never
 # expire and never change quality.
-class BlueDistinctionPlus
-  def update(award)
+class BlueDistinctionPlus < AwardCalculator
+  def update
     # Does nothing.
   end
 end
 
-class BlueFirst
-  def expired?(award)
-    award.expires_in < 0
+class BlueFirst < AwardCalculator
+  def initialize(award)
+    @award = award
   end
 
-  def increase_and_adjust_quality(award, n)
-    award.quality += 1
-    award.quality = 50 if award.quality > 50
+  def expired?
+    @award.expires_in < 0
   end
 
-  def update(award)
-    increase_and_adjust_quality award, 1
+  def increase_and_adjust_quality(n)
+    @award.quality += 1
+    @award.quality = 50 if @award.quality > 50
+  end
 
-    award.expires_in -= 1
+  def update
+    increase_and_adjust_quality 1
 
-    if expired?(award)
-      increase_and_adjust_quality award, 1
+    @award.expires_in -= 1
+
+    if expired?
+      increase_and_adjust_quality 1
     end
   end
 end
 
-class BlueCompare
-  def update(award)
-    if award.quality < 50
-      award.quality += 1
-      if award.name == 'Blue Compare'
-        if award.expires_in < 11
-          if award.quality < 50
-            award.quality += 1
+class BlueCompare < AwardCalculator
+  def update
+    if @award.quality < 50
+      @award.quality += 1
+      if @award.name == 'Blue Compare'
+        if @award.expires_in < 11
+          if @award.quality < 50
+            @award.quality += 1
           end
         end
 
-        if award.expires_in < 6
-          if award.quality < 50
-            award.quality += 1
+        if @award.expires_in < 6
+          if @award.quality < 50
+            @award.quality += 1
           end
         end
       end
     end
 
-    award.expires_in -= 1
+    @award.expires_in -= 1
 
-    if award.expires_in < 0
-      award.quality = 0
+    if @award.expires_in < 0
+      @award.quality = 0
     end
   end
 end
 
 def update_quality(awards)
+  calculator = AwardQualityCalculator.new
+
   awards.each do |award|
-    AwardQualityCalculator.new.update(award)
+    calculator.update(award)
   end
 end
